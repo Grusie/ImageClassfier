7261bd16a115af4be7db680f895872570c854c4b
Grusie <rbtjr02@naver.com>
2022-11-21 11:23:50 +0900

    modify structure activity_code_generator.xml, activity_save_variable.xml etc..

--- ImageClassifier/app/src/main/java/com/example/imageclassifier/CodeGenerator.java
+++ ImageClassifier/app/src/main/java/com/example/imageclassifier/CodeGenerator.java
@@ -1,55 +1,74 @@
 package com.example.imageclassifier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class CodeGenerator extends AppCompatActivity {
     TextView intentValue;
+    Button btnVariable1, btnVariable2, btnReCapture;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_generator);
 
-        ImageButton home1 = findViewById(R.id.home1);
-        home1.setOnClickListener(view -> {
-            Intent i = new Intent(CodeGenerator.this,MainActivity.class);
-            startActivity(i);
-        });
+        toolbar = findViewById(R.id.code_generator_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Button btnVariable1 = findViewById(R.id.btnVariable1);
-        btnVariable1.setOnClickListener(view -> {
-            Intent i = new Intent(CodeGenerator.this,Save_variable.class);
-            startActivity(i);
-        });
-
-        Button btnVariable2 = findViewById(R.id.btnVariable2);
-        btnVariable2.setOnClickListener(view -> {
-            Intent i = new Intent(CodeGenerator.this,Save_variable.class);
-            startActivity(i);
-        });
-
-        Button btnReCapture = findViewById(R.id.btnReCapture);
-        btnReCapture.setOnClickListener(view -> {
-            Intent i = new Intent(CodeGenerator.this, CameraActivity.class);
-            startActivity(i);
-        });
-
+        btnVariable1 = findViewById(R.id.btnVariable1);
+        btnVariable2 = findViewById(R.id.btnVariable2);
+        btnReCapture = findViewById(R.id.btnReCapture);
         Button btnToResult = findViewById(R.id.btnToResult);
+
+        ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            if (result.getData().getStringExtra("var") != null)
+                                btnVariable1.setText(result.getData().getStringExtra("var"));
+                            else
+                                btnVariable2.setText(result.getData().getStringExtra("var2"));
+                        }
+                    }
+                });
+
+        btnVariable1.setOnClickListener(view -> {
+            Intent i = new Intent(this,Save_variable.class);
+            i.putExtra("var", "");
+            startActivityResult.launch(i);
+        });
+
+        btnVariable2.setOnClickListener(view -> {
+            Intent i = new Intent(this,Save_variable.class);
+            i.putExtra("var2", "");
+            startActivityResult.launch(i);
+        });
+
+        btnReCapture.setOnClickListener(view -> {
+            Intent i = new Intent(this, CameraActivity.class);
+            startActivity(i);
+        });
+
         btnToResult.setOnClickListener(view -> {
-            Intent i = new Intent(CodeGenerator.this, Result.class);
+            Intent i = new Intent(this, Result.class);
             startActivity(i);
         });
 
@@ -59,6 +78,16 @@ public class CodeGenerator extends AppCompatActivity {
             intentValue = (TextView) findViewById(R.id.operator1);
             intentValue.setText(text);
 
+    }
 
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                finish();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
