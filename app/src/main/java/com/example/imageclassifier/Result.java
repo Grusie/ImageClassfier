aa62a33483ca4dc7a3c8221d8077632367815a48
grusie <rbtjr02@naver.com>
2022-11-28 15:34:24 +0900

    modify structure in activity_result.

--- app/src/main/java/com/example/imageclassifier/Result.java
+++ app/src/main/java/com/example/imageclassifier/Result.java
@@ -3,24 +3,58 @@ package com.example.imageclassifier;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Guideline;
 import androidx.core.widget.NestedScrollView;
 
 public class Result extends AppCompatActivity {
 
     int i = 1;
-    NestedScrollView nsc_view;
+    ConstraintLayout nsc_view;
     ImageButton btn_Hide;
+    Toolbar toolbar;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        toolbar = findViewById(R.id.activity_result_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("결과");
+
+        nsc_view = findViewById(R.id.Ctr_layout);
+        btn_Hide = findViewById(R.id.btn_Hide);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getRealSize(size); // or getSize(size)
+        int width = size.x;
+        int height = size.y;
+        nsc_view.setMaxHeight((int)(height * 0.7));
 
         Button btnHome = findViewById(R.id.btnHome);
         btnHome.setOnClickListener(view -> {
@@ -54,9 +88,6 @@ public class Result extends AppCompatActivity {
                 alert.show();
             }
         });
-
-        nsc_view = findViewById(R.id.nsc_view);
-        btn_Hide = findViewById(R.id.btn_Hide);
 
         nsc_view.setVisibility(View.VISIBLE);
 
